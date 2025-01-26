50e0ad555c0df4a1bc665449431e50cf9956d83a
UEDA Naoaki <nao.uedder@gmail.com>
2025-01-26 23:09:55 +0900

    Set org-archive-location env

--- conf/31-org-mode.el
+++ conf/31-org-mode.el
@@ -8,6 +8,17 @@
       '(("t" "TODO for me" entry (file+headline "~/org/agenda/me.org" "Inbox")
          "*** TODO %?\n    CAPTURED_AT: %a\n    %i")))
 
+(defun my/set-archive-location (&rest _)
+  "Set org-archive-location dynamically before archiving"
+  (let* ((date (format-time-string "%Y/%Y-%m"))
+         (archive-dir "~/org/archive")
+         (archive-path (format "%s/%s.org" archive-dir date)))
+    (make-directory (file-name-directory archive-path) t)
+    (setq org-archive-location (format "%s::" archive-path))))
+
+(advice-add 'org-archive-subtree :before #'my/set-archive-location)
+
+
 ; (setq org-log-done 'time)
 
 (org-babel-do-load-languages
