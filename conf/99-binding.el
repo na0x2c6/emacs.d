13523737ab787d8ab9d1686de31212fc743272fe
UEDA Naoaki <nao.uedder@gmail.com>
2025-01-29 09:57:59 +0900

    Add func and binding for copying filepath

--- conf/99-binding.el
+++ conf/99-binding.el
@@ -74,3 +74,51 @@
 
 ;; eldoc-box
 (define-key evil-normal-state-map (kbd "C-k") 'eldoc-box-help-at-point)
+
+(defun my/copy-buffer-file-path-to-clipboard (&optional arg)
+  "Copy the current buffer's file path to clipboard.
+With prefix ARG:
+  'h' - Copy path from home directory
+  'p' - Copy path from git project root (default)
+  'a' - Copy absolute path
+If buffer is not visiting a file, copy buffer name instead."
+  (interactive "P")
+  (let* ((file-path (or (buffer-file-name) (buffer-name)))
+         (path-type (if arg
+                       (read-char-choice
+                        "Copy path type [h]ome [p]roject [a]bsolute: "
+                        '(?h ?p ?a))
+                     ?p))
+         (result-path
+          (cond
+           ;; Buffer is not visiting a file
+           ((not (buffer-file-name))
+            (buffer-name))
+           
+           ;; Home directory path
+           ((eq path-type ?h)
+            (abbreviate-file-name file-path))
+           
+           ;; Project relative path
+           ((eq path-type ?p)
+            (if (and (fboundp 'project-current)
+                     (project-current))
+                (file-relative-name file-path
+                                  (project-root (project-current)))
+              (abbreviate-file-name file-path)))
+           
+           ;; Absolute path
+           ((eq path-type ?a)
+            (expand-file-name file-path))
+           
+           ;; Default case
+           (t file-path))))
+    
+    ;; Copy to clipboard
+    (with-temp-buffer
+      (insert result-path)
+      (clipboard-kill-ring-save (point-min) (point-max)))
+    
+    (message "Copied to clipboard: %s" result-path)))
+
+(global-set-key (kbd "C-c C-p") 'my/copy-buffer-file-path-to-clipboard)
