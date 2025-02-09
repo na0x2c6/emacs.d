f61fbce161e89e474980a62b10c1d63d3b38fd37
UEDA Naoaki <nao.uedder@gmail.com>
2025-02-09 18:50:50 +0900

    Fix grep-command default value assignment

--- conf/01-grep.el
+++ conf/01-grep.el
@@ -2,8 +2,8 @@
   "Set `grep-template' based on whether the current buffer is in a project."
   (cond
    ((project-current)
-      (grep-apply-setting 'grep-command '("git --no-pager grep -n -e ")))
+      (grep-apply-setting 'grep-command "git --no-pager grep -n -e "))
    (t
-      (grep-apply-setting 'grep-command '("grep --color=auto -nH --null -e ")))))
+      (grep-apply-setting 'grep-command "grep --color=auto -nH --null -e "))))
 
 (add-hook 'post-command-hook 'my/set-grep-template-based-on-project)
